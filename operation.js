b2c1426be35c8db6ea6b2f802857ab39a15ef990
Wahidul Hasan <52691980+piyaal@users.noreply.github.com>
2023-10-18 10:00:06 +0600

    Add files via upload
    shorthND

---
+++ operation.js
@@ -0,0 +1,4 @@
+var onion=30;
+var egg=20;
+console.log(onion);
+console.log(onion+egg);
---
+++ shorthand.js
@@ -0,0 +1,30 @@
+var price=20;
+var fname= 'kasem ';
+var lname='khan';
+var full=fname+'' +lname;
+console.log(fname+lname);
+
+var n='10';
+var m='20';
+var total= n+m;
+console.log(total);
+
+//ekta string ekta number thakleo sgtring akare add hbe
+var n=300;
+var m='20';
+var total= n+m;
+console.log(total);
+///string to number sonversion
+var n='10';
+var p=parseInt(n);
+console.log(p);
+
+
+
+
+//fixes number 
+var n=.300;
+var m=.2;
+var total= n+m;
+total=total.toFixed(2);
+console.log(total);
