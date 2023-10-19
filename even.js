1e6cf190ddcf44be2745a9885fe1c98742ae25e8
Wahidul Hasan <52691980+piyaal@users.noreply.github.com>
2023-10-19 10:11:52 +0600

    Add files via upload

---
+++ even.js
@@ -0,0 +1,17 @@
+var number=0;
+while(number<10){
+console.log(number);
+number++;
+
+}
+while(number<=20){
+    console.log(number);
+    number+=2;
+    
+    }
+
+    var n=1;
+    while(n<=20){
+        console.log(n);
+        n+=2;
+        }
