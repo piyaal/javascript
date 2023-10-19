41c139effec1413055dc78e2cb8895d381728247
Wahidul Hasan <52691980+piyaal@users.noreply.github.com>
2023-10-19 12:20:43 +0600

    Add files via upload

---
+++ function.js
@@ -0,0 +1,7 @@
+function start(){
+console.log('stand up');
+console.log("turn on the light");
+}
+//call the function 
+start();
+start();
---
+++ return.js
@@ -0,0 +1,6 @@
+function add(n1,n2) {
+console.log(n1,n2);
+var sum=n1+n2;
+console.log(sum);
+return sum; }
+add(50,60);
