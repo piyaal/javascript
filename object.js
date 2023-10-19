964212d826f42402a7cacb45a330216ea1b4b22d
Wahidul Hasan <52691980+piyaal@users.noreply.github.com>
2023-10-19 12:36:53 +0600

    Add files via upload

---
+++ object.js
@@ -0,0 +1,22 @@
+ var student ={
+  id: 48,
+  name: 'hasan',
+  class:12,
+  marks:90
+
+ }
+ var mobile = {
+    brand: 'Realme',
+    camera: '32mp', 
+    storage: '64gb'
+  
+   }
+   console.log(mobile);
+   var pc={
+
+    brande: 'hp',
+    price: 50000,
+    color:'silver',
+    processor: 'i5'
+   }
+   console.log(pc);
