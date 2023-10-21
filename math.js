0f7228e3646f3cf5c70b5cb7a15dc0f55b8e5cdf
Wahidul Hasan <52691980+piyaal@users.noreply.github.com>
2023-10-21 12:43:40 +0600

    Add files via upload

---
+++ math.js
@@ -0,0 +1,40 @@
+const result= Math.pow(2,4);
+console.log(result);
+
+const n1=20;
+const n2=30;
+ const res= Math.abs(n1-n2);
+ console.log(res);
+ if(res<5)
+
+ console.log("good");
+ else
+ console.log("bad");
+
+ const p=3.141617;
+ const p1=Math.round(p);
+ console.log(p1);
+
+
+ const x=3.141617;
+ const x1=Math.ceil(x);
+ console.log(x1);
+
+
+ const y=3.141617;
+ const y1=Math.floor(y);
+ console.log(y1);
+
+//swap with temp
+ let xx=10; let yy=30;
+ console.log(xx,yy);
+ let temp=xx;
+ xx=yy;
+ yy=temp;
+ console.log(xx,yy);
+
+ //swap withoout temp
+ let aa=100; let bb=300;
+ [aa,bb]=[bb,aa];
+ console.log(aa,bb);
+
